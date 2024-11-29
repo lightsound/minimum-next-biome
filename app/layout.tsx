b2ec9810ca87fefb909cffe21eeb8f11bb22f5bd
しまぶー <0120koto@gmail.com>
2024-11-29 16:56:06 +0900

    🎨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import "./globals.css";
-
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}: { children: React.ReactNode }): React.ReactNode {
 	return (
 		<html lang="ja">
-			<body>{children}</body>
+			<body suppressHydrationWarning={true}>{children}</body>
 		</html>
 	);
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main>
-			<div className="bg-red-200 p-4 text-4xl text-red-900">hello</div>
-		</main>
-	);
-}
